23858aef41c70d74fb92ec5b869e1d642ecc651d
Bennett Moore <bennettmoore05@gmail.com>
2025-11-12 11:01:50 -0500

    docs: comments on frontend API utils

--- frontend/src/api/utils.ts
+++ frontend/src/api/utils.ts
@@ -1,10 +1,14 @@
 import normalizeUrl from 'normalize-url';
 
+// Normalize an API route before sending it, just in case there's something wrong with how it was passed.
+// This function also makes it so we only have to import the backend URL env variable in one place.
 export function apiRoute(endpoint: string): string {
   const raw = import.meta.env.VITE_BACKEND_URL! + endpoint;
   return normalizeUrl(raw);
 }
 
+// Define the headers used for JWT authentication in the backend.
+// This reduces redundant code.
 export function jwtHeaders(token: string): Record<string, string> {
   return {
     Authorization: `Bearer ${token}`,
