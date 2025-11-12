81e2ba4335eafa7d53fd5d12225a1da90a608d2b
Bennett Moore <bennettmoore05@gmail.com>
2025-11-12 10:55:02 -0500

    docs: comments in shared pkg

--- shared/src/auth.ts
+++ shared/src/auth.ts
@@ -1,4 +1,5 @@
+// This interface defines the data encoded in JWT tokens.
 export interface TokenPayload {
-  sub: string;
-  role: string;
+  sub: string; // User ID
+  role: string; // User's RBAC role
 }
