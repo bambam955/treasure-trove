92f5ba1484e9fb2fc435da7570f73aa47f16f991
Bennett Moore <bennettmoore05@gmail.com>
2025-11-14 11:17:33 -0500

    feat: start implementing auctions frontend API

---
+++ frontend/src/api/auctions.ts
@@ -0,0 +1,40 @@
+/// <reference types="vite/client" />
+
+import { apiRoute, jwtHeaders } from './utils';
+import { AuctionInfo } from '@shared/auctions.ts';
+
+class AuctionsApi {
+  // TODO: get all auctions, get one auction
+
+  static async createAuction(
+    auctionInfo: AuctionInfo,
+    token: string,
+  ): Promise<AuctionInfo> {
+    const res = await fetch(apiRoute('auctions'), {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', ...jwtHeaders(token) },
+      body: JSON.stringify(auctionInfo),
+    });
+    if (!res.ok) throw new Error('failed to create auction');
+    return await res.json();
+  }
+
+  static async updateAuction(
+    id: string,
+    auction: Partial<AuctionInfo>,
+    token: string,
+  ): Promise<AuctionInfo> {
+    const res = await fetch(apiRoute(`auctions/${id}`), {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        ...jwtHeaders(token),
+      },
+      body: JSON.stringify(auction),
+    });
+    if (!res.ok) throw new Error('failed to update auction');
+    return await res.json();
+  }
+}
+
+export default AuctionsApi;
