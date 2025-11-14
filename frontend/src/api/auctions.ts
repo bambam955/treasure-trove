aa3d8ceaff077fd9d2acc489a79097d5759eb2f0
Bennett Moore <bennettmoore05@gmail.com>
2025-11-14 17:58:22 -0500

    feat: implement GETs for auctions frontend API

--- frontend/src/api/auctions.ts
+++ frontend/src/api/auctions.ts
@@ -5,6 +5,29 @@ import { AuctionInfo } from '@shared/auctions.ts';
 
 class AuctionsApi {
   // TODO: get all auctions, get one auction
+  static async getAllAuctions(token: string): Promise<AuctionInfo[]> {
+    const res = await fetch(apiRoute('auctions'), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        ...jwtHeaders(token),
+      },
+    });
+    if (!res.ok) throw new Error('failed to fetch auctions');
+    return await res.json();
+  }
+
+  static async getAuctionInfo(id: string, token: string): Promise<AuctionInfo> {
+    const res = await fetch(apiRoute(`auctions/${id}`), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        ...jwtHeaders(token),
+      },
+    });
+    if (!res.ok) throw new Error('failed to fetch auction');
+    return await res.json();
+  }
 
   static async createAuction(
     auctionInfo: AuctionInfo,
