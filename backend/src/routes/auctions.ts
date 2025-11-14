a98fe39558e7bc5348dcf6a6af8ff78b6036730b
Bennett Moore <bennettmoore05@gmail.com>
2025-11-14 11:11:32 -0500

    feat: implement POST and PUT routes for auctions

--- backend/src/routes/auctions.ts
+++ backend/src/routes/auctions.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import { requireAuth } from '../middleware/jwt.ts';
 import AuctionsService from 'src/services/auctions.ts';
+import { auctionInfoSchema } from '@shared/auctions.ts';
 
 const auctionsRouter = express.Router();
 
@@ -25,16 +26,33 @@ auctionsRouter.get('/:id', requireAuth, async (req: Request, res: Response) => {
   }
 });
 
-auctionsRouter.post('/', requireAuth, async (_req: Request, res: Response) => {
-  res.status(200);
+auctionsRouter.post('/', requireAuth, async (req: Request, res: Response) => {
+  try {
+    const validatedBody = auctionInfoSchema.validateSync(req.body);
+    const auction = AuctionsService.createAuction(validatedBody);
+    // Use 201 when a POST request successfully creates a new resource on the server.
+    return res.status(201).json(auction);
+  } catch (error) {
+    console.error('Auction creation error:', error);
+    return res.status(400).json({
+      error: 'Failed to create auction',
+    });
+  }
 });
 
-auctionsRouter.put(
-  '/:id',
-  requireAuth,
-  async (_req: Request, res: Response) => {
-    res.status(200);
-  },
-);
+auctionsRouter.put('/:id', requireAuth, async (req: Request, res: Response) => {
+  try {
+    const validatedBody = auctionInfoSchema.validateSync(req.body);
+    const auctionInfo = await AuctionsService.updateAuction(
+      req.params.id,
+      validatedBody,
+    );
+    return res.status(200).json(auctionInfo);
+  } catch (error) {
+    console.error('Error updating auction:', error);
+    // Use 400 when there is a bad request for some reason.
+    return res.status(400).json({ error: 'failed to update auction ' });
+  }
+});
 
 export default auctionsRouter;
