ec69fa295e895366c9153508c218e6db5f5ec48d
Bennett Moore <bennettmoore05@gmail.com>
2025-11-14 23:13:44 -0500

    chore: get rid of test code in Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,18 +1,8 @@
 import { useAuth } from '../contexts/AuthContext';
 import { Header } from '../components/Header';
-import { AuctionInfo } from '@shared/auctions.ts';
-import { useQuery } from '@tanstack/react-query';
-import AuctionsApi from '../api/auctions.ts';
 
 export function Home() {
   const [token] = useAuth();
-
-  const auctionsQuery = useQuery<AuctionInfo[]>({
-    queryKey: ['users'],
-    queryFn: () => AuctionsApi.getAllAuctions(token!),
-  });
-  // auctionsQuery.refetch();
-  console.log('auctions are:', auctionsQuery.data);
 
   // If the user goes to this page without being logged in then show an error message.
   if (!token) {
@@ -32,23 +22,7 @@ export function Home() {
     <div className='vh-100 d-flex flex-column p-2'>
       <Header />
       <div className='flex-grow-1 d-flex align-items-center justify-content-center'>
-        {/* <div>Content coming soon!</div> */}
-        <table>
-          <tbody>
-            {auctionsQuery.data?.map((a) => (
-              <tr key={a.id}>
-                <td>{a.title}</td>
-                <td>{a.description}</td>
-                <td>{a.buyerId}</td>
-                <td>{a.sellerId}</td>
-                <td>{a.minimumBid}</td>
-                {/* <td>{new Date(a.endDate).toISOString()}</td> */}
-                <td>{a.endDate.toISOString()}</td>
-                <td>{a.expectedValue}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div>Content coming soon!</div>
       </div>
     </div>
   );
