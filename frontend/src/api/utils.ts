8fae7ec50551ed47730037cd683503dd2d5a7f64
Bennett Moore <bennettmoore05@gmail.com>
2025-11-30 22:25:51 -0500

    fix: try hardcoding api routes

--- frontend/src/api/utils.ts
+++ frontend/src/api/utils.ts
@@ -3,8 +3,8 @@ import normalizeUrl from 'normalize-url';
 // Normalize an API route before sending it, just in case there's something wrong with how it was passed.
 // This function also makes it so we only have to import the backend URL env variable in one place.
 export function apiRoute(endpoint: string): string {
-  const raw = import.meta.env.VITE_BACKEND_URL! + endpoint;
-  return normalizeUrl(raw);
+  const base = import.meta.env.VITE_BACKEND_URL ?? '/api/v1/';
+  return normalizeUrl(base + endpoint);
 }
 
 // Define the headers used for JWT authentication in the backend.
